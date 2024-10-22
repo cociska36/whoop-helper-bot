cd045ddd40dc71e9e9b82dd02c6665d7f748df1a
cociska36 <150147730+cociska36@users.noreply.github.com>
2024-10-22 06:48:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Константы для OAuth 2.0
-CLIENT_ID = "88b93373-32cc-433b-9f02-7acf0c1a8f35"
-CLIENT_SECRET = "53c824ce8b9572cfa0cc56967b6f5c60d53676c53e8dcec9aacf1026ffa8096d"
-REDIRECT_URI = "https://www.whoop.com/"
+CLIENT_ID = "your_client_id"
+CLIENT_SECRET = "your_client_secret"
+REDIRECT_URI = "your_redirect_url"
 AUTH_URL = "https://api.prod.whoop.com/oauth/oauth2/auth"
 TOKEN_URL = "https://api.prod.whoop.com/oauth/oauth2/token"
-SCOPE = "offline read:recovery read:cycles read:workout read:sleep read:profile read:body_measurement"
+SCOPE = "scope_parametrs"
 TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
